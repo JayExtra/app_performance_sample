047f10d563176293f0ecaf1349bca93c0193dde0
James Gitonga <40741293+JayExtra@users.noreply.github.com>
2023-11-03 15:29:03 +0300

    benchmark tests - firebase connection for CI benchmarks

--- app/src/main/java/com/article/appperformancesample/MainActivity.kt
+++ app/src/main/java/com/article/appperformancesample/MainActivity.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.metrics.performance.JankStats
 import com.article.appperformancesample.ui.theme.AppPerformanceSampleTheme
 
 class MainActivity : ComponentActivity() {
